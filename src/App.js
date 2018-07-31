d1b088d6b8f86924a159015a1901784645e68f9e
beth_becerra <beth.bcerra@gmail.com>
2018-07-31 22:16:25 +0200

    add creative commons

--- src/App.js
+++ src/App.js
@@ -16,9 +16,19 @@ class App extends Component {
         </header>
         <div>{<Images />}</div>
         <footer>
-          <a className="Contact-instagram" href="https://www.instagram.com/beth_becerra/">
-            Free download by @beth_becerra
+          <a rel="license" href="http://creativecommons.org/licenses/by-nd/4.0/">
+            <img
+              className="img_license"
+              alt="Creative Commons License"
+              src="https://i.creativecommons.org/l/by-nd/4.0/88x31.png"
+            />
           </a>
+          <br />This work is licensed under a{' '}
+          <a rel="license" href="http://creativecommons.org/licenses/by-nd/4.0/">
+            Creative Commons Attribution-NoDerivatives 4.0 International License
+          </a>
+          {'  '}
+          | Create by <a href="https://www.instagram.com/beth_becerra/">@beth_becerra</a>
         </footer>
       </div>
     );
